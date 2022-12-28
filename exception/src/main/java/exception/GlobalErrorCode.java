75c6b69e27ccf9a070aae1cd47ab25d60b4c1508
VincentWang1024 <v1ncent.wang1024@gmail.com>
2022-12-28 17:22:46 +0000

    3

--- exception/src/main/java/exception/GlobalErrorCode.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class GlobalErrorCode {
-    public static final String ERROR_ENTITY_NOT_FOUND = "BANKING-CORE-SERVICE-1000";
-    public static final String ERROR_INVALID_NAME = "BANKING-CORE-SERVICE-1001";
-    public static final String ERROR_INVALID_LicenseNo = "BANKING-CORE-SERVICE-1002";
-}
--- exception/src/main/java/exception/InvalidLicenseNoException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidLicenseNoException extends SimpleBankingGlobalException {
-    public InvalidLicenseNoException(String message, String code) {
-        super(message, code);
-    }
-}
